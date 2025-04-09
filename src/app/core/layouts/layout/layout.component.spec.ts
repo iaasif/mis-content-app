35a375500c43eae24e2e82f2a8bfe1530151a6c9
Fazlul619 <fazlul15-2569@diu.edu.bd>
2025-04-09 18:24:13 +0600

    range slider,numaric only deractive and input issue fix

--- src/app/core/layouts/layout/layout.component.spec.ts
+++ src/app/core/layouts/layout/layout.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
-import { MessageBoxService } from '../../services/messageBox/message-box.service';
 import { ConfirmationModalService } from '../../services/confirmationModal/confirmation-modal.service';
 import { ModalService } from '../../services/modal/modal.service';
 import { CircularLoaderService } from '../../services/circularLoader/circular-loader.service';
@@ -15,7 +14,6 @@ describe('LayoutComponent', () => {
     await TestBed.configureTestingModule({
       imports: [LayoutComponent],
       providers: [
-        MessageBoxService,
         ConfirmationModalService,
         ModalService,
         CircularLoaderService,
