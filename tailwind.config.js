1e04538971eda1d9e284a43119d23578a9c2b2de
iaasif <imtiazasif.cse@gmail.com>
2026-01-28 14:37:44 +0600

    feat: update layout and navigation components, add Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts,css}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,css,html}"],
   theme: {
     extend: {},
   },
