5a8e8e3cff31e5217ff198b053b7db1d8888fc90
iaasif <imtiazasif.cse@gmail.com>
2026-02-03 12:43:42 +0600

    feat: integrate ngxsmk-datepicker component for date selection in edit job form
    - Added ngxsmk-datepicker to package.json and package-lock.json.
    - Updated edit-job.html to include date pickers for 'From Date' and 'To Date'.
    - Modified edit-job.ts to manage date values using signals.

--- src/app/features/pages/edit-job/edit-job.ts
+++ src/app/features/pages/edit-job/edit-job.ts
@@ -1,14 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { DropdownComponent } from "../../../shared/components/dropdown-component/dropdown-component";
 import { priorities } from '../mis/utils/mis.data';
+import { DatepickerValue, NgxsmkDatepickerComponent } from 'ngxsmk-datepicker';
 
 @Component({
   selector: 'app-edit-job',
-  imports: [DropdownComponent],
+  imports: [DropdownComponent,NgxsmkDatepickerComponent],
   templateUrl: './edit-job.html',
   styleUrl: './edit-job.css',
 })
 export class EditJob {
-  position = signal(priorities)
+  position = signal(priorities);
+  FromDate = signal<DatepickerValue>(null);
+  ToDate = signal<DatepickerValue>(null);
 
-}
+} 
