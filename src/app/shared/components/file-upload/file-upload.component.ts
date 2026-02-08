ee8452ca9297d18b33a1dbce4dbd069a158235ff
iaasif <imtiazasif.cse@gmail.com>
2026-02-08 12:53:07 +0600

    upload progress bar working

--- src/app/shared/components/file-upload/file-upload.component.ts
+++ src/app/shared/components/file-upload/file-upload.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpResponse } from '@angular/common/http';
 import { AfterViewChecked, Component, ElementRef, EventEmitter, inject, OnChanges, Output, signal, SimpleChanges, viewChild, input } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { delay, of } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 export const DefaultMaxSize = 9000000;
 
@@ -144,85 +145,108 @@ export class FileUploadComponent implements AfterViewChecked, OnChanges {
     this.uploadInProgress.emit(isVisible);
   }
 
-  upload2() {
-    if (this.file instanceof File) {
-      this.toggleUploadProgress(true);
-      const formData = new FormData();
+  // upload2() {
+  //   if (this.file instanceof File) {
+  //     this.toggleUploadProgress(true);
+  //     const formData = new FormData();
 
-      formData.append('ImgFile', this.file);
-      const propKeys = Object.keys(this.payload());
-      if (propKeys.length) {
-        for (let index = 0; index < propKeys.length; index++) {
-          const key = propKeys[index];
-          formData.append(key, this.payload()[key] as string);
-        }
-      }
-      // previous link 'https://exam1.bdjobs.com/ExamImages/Corporate/Test_Img_Saving-tst.aspx',
-      const xhr = new XMLHttpRequest();
-      xhr.open(
-        'POST',
-        'https://exam1.bdjobs.com/ExamImages/Corporate/Test_Img_Saving-tst.aspx',
-        true
-      );
+  //     formData.append('ImgFile', this.file);
+  //     const propKeys = Object.keys(this.payload());
+  //     if (propKeys.length) {
+  //       for (let index = 0; index < propKeys.length; index++) {
+  //         const key = propKeys[index];
+  //         formData.append(key, this.payload()[key] as string);
+  //       }
+  //     }
+  //     // previous link 'https://exam1.bdjobs.com/ExamImages/Corporate/Test_Img_Saving-tst.aspx',
+  //     const xhr = new XMLHttpRequest();
+  //     xhr.open(
+  //       'POST',
+  //       'https://exam1.bdjobs.com/ExamImages/Corporate/Test_Img_Saving-tst.aspx',
+  //       true
+  //     );
 
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-          if (xhr.status === 200 || xhr.status === 201) {
-            this.uploadResult = 'Uploaded';
-            this.isPreview.update(() => false);
-            const url = `https://exam1.bdjobs.com/ExamImages/Corporate/JID_${this.payload()['hJID']}/${this.payload()['hImgName']}.jpg`;
-            of(url)
-              .pipe(delay(1000))
-              .subscribe({
-                next: (url) => {
-                  this.onSuccess.emit(url);
-                  this.uploadInProgress.emit(false);
-                },
-              });
-          } else if (xhr.status === 400) {
-            this.uploadResult = JSON.parse(xhr.response)!.message;
-          } else {
-            this.uploadResult = 'File upload failed!';
-          }
-          this.uploadStatus.update(() => (xhr.status === 0 ? 1 : xhr.status));
-        }
-      };
+  //     xhr.onreadystatechange = () => {
+  //       if (xhr.readyState === XMLHttpRequest.DONE) {
+  //         if (xhr.status === 200 || xhr.status === 201) {
+  //           this.uploadResult = 'Uploaded';
+  //           this.isPreview.update(() => false);
+  //           const url = `https://exam1.bdjobs.com/ExamImages/Corporate/JID_${this.payload()['hJID']}/${this.payload()['hImgName']}.jpg`;
+  //           of(url)
+  //             .pipe(delay(1000))
+  //             .subscribe({
+  //               next: (url) => {
+  //                 this.onSuccess.emit(url);
+  //                 this.uploadInProgress.emit(false);
+  //               },
+  //             });
+  //         } else if (xhr.status === 400) {
+  //           this.uploadResult = JSON.parse(xhr.response)!.message;
+  //         } else {
+  //           this.uploadResult = 'File upload failed!';
+  //         }
+  //         this.uploadStatus.update(() => (xhr.status === 0 ? 1 : xhr.status));
+  //       }
+  //     };
 
-      xhr.upload.onprogress = (progressEvent) => {
-        this.toggleUploadProgress(true);
-        if (progressEvent.lengthComputable) {
-          const progress = (progressEvent.loaded / progressEvent.total) * 100;
-          this.progress.update(() => `${Math.round(progress)}%`);
-        }
-      };
+  //     xhr.upload.onprogress = (progressEvent) => {
+  //       this.toggleUploadProgress(true);
+  //       if (progressEvent.lengthComputable) {
+  //         const progress = (progressEvent.loaded / progressEvent.total) * 100;
+  //         this.progress.update(() => `${Math.round(progress)}%`);
+  //       }
+  //     };
 
-      xhr.send(formData);
-    }
-  }
+  //     xhr.send(formData);
+  //   }
+  // }
   /** The file is sent in the request body as FormData (headers cannot contain binary data). */
   upload() {
     if (!this.file) return;
     this.toggleUploadProgress(true);
+    this.progress.set('0%');
     const form = new FormData();
     form.append('id', String(this.payload()['id'] ?? '877866'));
     form.append('imageName', String(this.payload()['imageName'] ?? 'HotJobLogo'));
     form.append('Image', this.file, this.file.name);
-    this.http.post<{ id: string; profile?: string }>(this.apiUrl, form).subscribe({
-      next: (res) => {
-        console.log('res',res)
-        this.uploadResult = 'Uploaded';
-        this.uploadStatus.update(() => 200);
-        this.isPreview.update(() => false);
-        this.onSuccess.emit(res.profile ?? res.id);
-        this.toggleUploadProgress(false);
-      },
-      error: (err) => {
-        console.log('err',err)
-        this.uploadResult = err.error?.message ?? 'File upload failed!';
-        this.uploadStatus.update(() => err.status ?? 500);
-        this.toggleUploadProgress(false);
-      },
-    });
+    this.http
+      .post<{ id: string; profile?: string }>(this.apiUrl, form, {
+        reportProgress: true,
+        observe: 'events',
+      })
+      .pipe(
+        tap((event) => {
+          if (
+            event.type === HttpEventType.UploadProgress &&
+            event.loaded != null &&
+            event.total != null &&
+            event.total > 0
+          ) {
+            this.progress.set(
+              `${Math.round((100 * event.loaded) / event.total)}%`
+            );
+          }
+        }),
+        filter((event) => event.type === HttpEventType.Response),
+        map((event) =>
+          (event as HttpResponse<{ id: string; profile?: string }>).body
+        )
+      )
+      .subscribe({
+        next: (res) => {
+          if (!res) return;
+          this.uploadResult = 'Uploaded';
+          this.uploadStatus.set(200);
+          this.isPreview.set(false);
+          this.onSuccess.emit(res.profile ?? res.id);
+          this.toggleUploadProgress(false);
+        },
+        error: (err) => {
+          this.uploadResult = err.error?.message ?? 'File upload failed!';
+          this.uploadStatus.set(err.status ?? 500);
+          this.toggleUploadProgress(false);
+        },
+      });
   }
 
   cancel() {
