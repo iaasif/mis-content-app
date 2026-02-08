8074722333de80343175e1d572d20276286ded3f
iaasif <imtiazasif.cse@gmail.com>
2026-02-08 18:16:11 +0600

    update

--- src/app/shared/components/text-filed/text-filed.ts
+++ src/app/shared/components/text-filed/text-filed.ts
@@ -1,4 +1,6 @@
-import { Component, input } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, input, PLATFORM_ID, signal } from '@angular/core';
+import { platformBrowser } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-text-filed',
@@ -7,5 +9,22 @@ import { Component, input } from '@angular/core';
   styleUrl: './text-filed.css',
 })
 export class TextFiled {
+  private platformId = inject(PLATFORM_ID);
+  isBrowser = isPlatformBrowser(this.platformId);
+
+
   text = input('')
+  text2 = 'https://storage.googleapis.com/bdjobs/CompanyLogos/Hotjobs/877866_300x300_pexels-kim-438153-1159976.jpg'
+  isShowTick=signal(false);
+
+  onClickCopy():void{
+    console.log('onClickCopy function called!');
+    console.log('isBrowser:', this.isBrowser);
+    if(this.isBrowser){
+      this.isShowTick.set(true)
+      setTimeout(() => {
+        this.isShowTick.set(false)
+      }, 4000);
+    }
+  }
 }
