bc2d30bab119d2e2fd3c983a762d2b9f12581694
iaasif <imtiazasif.cse@gmail.com>
2026-03-30 10:52:08 +0600

    ok

--- src/app/features/pages/mis/components/rearrange-hot-job/rearrange-hot-job.ts
+++ src/app/features/pages/mis/components/rearrange-hot-job/rearrange-hot-job.ts
@@ -2,38 +2,35 @@ import {
   CdkDrag,
   CdkDragDrop,
   CdkDropList,
-  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
 } from '@angular/cdk/drag-drop';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RearrangeData } from '../../utils/mis.data';
 import { MisApi } from '../../services/mis-api';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { HotJob } from '../../models/jobs.data';
-import { tap } from 'rxjs';
-
-function distribute<T>(items: T[], columns: number): T[][] {
-  const result: T[][] = Array.from({ length: columns }, () => []);
-  items.forEach((item, i) => result[i % columns].push(item));
-  return result;
-}
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-rearrange-hot-job',
   standalone: true,
-  imports: [CdkDropListGroup, CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './rearrange-hot-job.html',
   styleUrl: './rearrange-hot-job.css',
 })
-export class RearrangeHotJob implements OnInit {
-  private misService = inject(MisApi);
+export class RearrangeHotJob {
+  // col1 = [...RearrangeData]
+  col1 = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  col2 = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  col3 = ['good', 'bad', 'joss', 'nice', 'lol'];
+  col4 = ['one', 'two', 'three', 'four', 'five'];
 
-  private source: string[] = [
-    'Get to work', 'Pick up groceries', 'Go home', 'Fall asleep',
-    'Get up', 'Brush teeth', 'Take a shower', 'Check e-mail',
-    'Walk dog', 'good', 'bad', 'joss', 'nice', 'lol',
-    'one', 'two', 'three', 'four', 'five',
-  ];
+  misService = inject(MisApi)
 
-  columns: string[][] = [];
+  ngOnInit(): void {
+
+  }
 
   hotJobs = toSignal(
     this.misService.getHotJobs().pipe(
@@ -41,26 +38,16 @@ export class RearrangeHotJob implements OnInit {
     ),
     { initialValue: [] as HotJob[] }
   );
-
-  ngOnInit(): void {
-    this.columns = distribute(this.source, 4);
-  }
-
-  noEnter = () => false;
-
-  drop(event: CdkDragDrop<string[]>): void {
+  drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      const data = [...event.container.data];
-      [data[event.previousIndex], data[event.currentIndex]] =
-        [data[event.currentIndex], data[event.previousIndex]];
-      this.columns[this.columns.indexOf(event.container.data)] = data;
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      const from = [...event.previousContainer.data];
-      const to = [...event.container.data];
-      [from[event.previousIndex], to[event.currentIndex]] =
-        [to[event.currentIndex], from[event.previousIndex]];
-      this.columns[this.columns.indexOf(event.previousContainer.data)] = from;
-      this.columns[this.columns.indexOf(event.container.data)] = to;
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
     }
   }
 }
