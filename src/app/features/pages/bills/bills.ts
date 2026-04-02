6940ab4037885ce898a1cf18338d8a0de6ab89d2
iaasif <imtiazasif.cse@gmail.com>
2026-04-02 14:17:12 +0600

    working

--- src/app/features/pages/bills/bills.ts
+++ src/app/features/pages/bills/bills.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
-
+interface BillRow {
+  id: number;
+  date: string;
+  from: string;
+  to: string;
+  company: string;
+  purpose: string;
+  transport: string;
+  amount: number;
+}
 @Component({
   selector: 'app-bills',
   imports: [],
