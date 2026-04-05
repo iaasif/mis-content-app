00eed3f1ba550e1dfe33fed851256189f10b4e22
iaasif <imtiazasif.cse@gmail.com>
2026-04-05 12:51:35 +0600

    update some file and fix a issue

--- src/app/features/pages/mis/components/add-company/add-company.ts
+++ src/app/features/pages/mis/components/add-company/add-company.ts
@@ -1,17 +1,15 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HotJobCategory, HotJobType, priorities } from '../../utils/mis.data';
 import { InputComponent } from '../../../../../shared/components/input/input.component';
-import { FileUploadComponent } from "../../../../../shared/components/file-upload/file-upload.component";
 import { MisApi } from '../../services/mis-api';
 import { CreateCompany } from '../../models/jobs.data';
 import { HotToastService } from '@ngxpert/hot-toast';
 import { StoreDataService } from '../../services/store-data-service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-company',
-  imports: [ReactiveFormsModule, InputComponent, FileUploadComponent],
+  imports: [ReactiveFormsModule, InputComponent],
   templateUrl: './add-company.html',
   styleUrl: './add-company.css',
 })
