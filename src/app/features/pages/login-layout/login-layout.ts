8cac915541bac52e8036d60ab4658b9f6ce2b989
iaasif <imtiazasif.cse@gmail.com>
2026-05-11 15:22:20 +0600

    feat: add new login layout component with HTML, CSS, and unit tests

---
+++ src/app/features/pages/login-layout/login-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-layout',
+  imports: [],
+  templateUrl: './login-layout.html',
+  styleUrl: './login-layout.css',
+})
+export class LoginLayout {
+
+}
